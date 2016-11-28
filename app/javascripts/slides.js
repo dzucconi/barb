4553b7c7c67165b16fb8bff75adcb30852ba7607
dzucconi <mail@damonzucconi.com>
2016-11-28 20:51:20 +0100

    Adds image example

--- app/javascripts/slides.js
+++ app/javascripts/slides.js
@@ -1,8 +1,14 @@
+import * as cards from './cards';
+
 const examples = [
   {
     text: () => 'Goodbye world.',
     note: 'This is a note.',
   },
+
+  cards.image('https://atlas-production.s3.amazonaws.com/7709/6cee74a22b4cdf000819aab8195ac5de068d626aa3a158fa8401818ed385b335.jpg', {
+    title: 'An image',
+  }),
 ];
 
 const slides = [
